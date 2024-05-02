17805f292fe46c4b7f33019c99f9582c98da350c
Arjun Singh <arjun2314singh@gmail.com>
2024-05-02 23:44:03 +0530

    Delete OnlineProductStore/src/main directory

--- OnlineProductStore/src/main/java/online/product/store/DBInfo.java
+++
@@ -1,9 +0,0 @@
-package online.product.store;
-
-public interface DBInfo 
-{
-	public static final String dbUrl="jdbc:oracle:thin:@localhost:1522:orcle";
-	public static final String uName="system";
-	public static final String pWord="Arjun2611";
-
-}
